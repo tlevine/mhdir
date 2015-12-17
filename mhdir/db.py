193527c64cccceb20b1d35c6bb3587ffa037b46f
Thomas Levine <_@thomaslevine.com>
2015-12-17 06:48:47 -0500

    Automatic commit with j

--- mhdir/db.py
+++ mhdir/db.py
@@ -1,5 +1,5 @@
 '''
-The database directory has this structure. ::
+The database maildir has this structure. ::
 
     ./current -> ./folders/Lists
     ./folders/INBOX/ # some-message-id
@@ -11,16 +11,16 @@ import os
 
 class Current(object):
 
-    def __init__(self, directory):
-        os.makedirs(directory, exist_ok = True)
-        self._directory = directory
+    def __init__(self, maildir):
+        os.makedirs(maildir, exist_ok = True)
+        self._maildir = maildir
 
     def __repr__(self):
-        return 'Current(%s)' % repr(self._directory)
+        return 'Current(%s)' % repr(self._maildir)
 
     @property
     def _folder_path(self):
-        return os.path.join(self._directory, '.current')
+        return os.path.join(self._maildir, '.mhdir-current-folder')
 
     @property
     def folder(self):
@@ -28,16 +28,19 @@ class Current(object):
             return os.readlink(self._folder_path)
 
     @folder.setter
-    def folder(self, dst):
-        if '/' in dst:
-            raise ValueError('Folder must not contain slashes.')
-        if os.path.islink(self._folder_path):
-            os.unlink(self._folder_path)
-        os.symlink(os.path.join(self._directory, dst), self._folder_path)
+    def folder(self, src):
+        if '/' in src:
+            raise ValueError('Folder name may not contain a slash.')
+
+        if os.path.isdir(os.path.join(self._maildir, src)):
+            if os.path.islink(self._folder_path):
+                os.unlink(self._folder_path)
+            os.symlink(src, self._folder_path)
+        else:
+            raise NotADirectoryError(src)
 
     @property
     def _message_path(self):
-        print(self._folder_path)
         return os.readlink(self._folder_path)
 
     @property
@@ -51,4 +54,7 @@ class Current(object):
             os.unlink(self._message_path)
         os.symlink(dst, self._message_path)
 
-c = Current('/tmp/blah')
+c = Current('/Users/t/tom/maildir/hot/_@thomaslevine.com/')
+print(c.folder)
+# c.folder = '/Users/t/tom/maildir/hot/_@thomaslevine.com/INBOX'
+c.folder = 'INBOX'
