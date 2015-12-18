071f3ec0a8ac740191400e21d1e0044b0c434171
Thomas Levine <_@thomaslevine.com>
2015-12-18 07:50:01 -0500

    new -> cur

--- mhdir/inc.py
+++ mhdir/inc.py
@@ -6,6 +6,14 @@ from .db import CSVMap
 
 MAILDIR = Path('/Users/t/tom/maildir/hot/_@thomaslevine.com/')
 
+
+PASSED = 'P' # the user has resent/forwarded/bounced this message to someone else.
+REPLIED = 'R' # the user has replied to this message.
+SEEN = 'S' # the user has viewed this message, though perhaps he didn't read all the way through it.
+TRASHED = 'T' # the user has moved this message to the trash; the trash will be emptied by a later user action.
+DRAFT = 'D' # the user considers this message a draft; toggled at user discretion.
+FLAGGED = 'F' # user-defined flag; toggled at user discretion. 
+
 def inc():
     '''
     1. If the message-id lookup doesn't exist, read everything in "cur" and add to the message-id lookup cache.
@@ -14,6 +22,16 @@ def inc():
     messageid_path = CSVMap(os.path.expanduser('~/.mhdir-messageid-path'))
     path_messageid = CSVMap(os.path.expanduser('~/.mhdir-path-messageid'))
     for folder in MAILDIR.iterdir():
+
+        # Move stuff from new to cur.
+        for mail in (folder / 'new'):
+            if ':' in mail.name:
+                newname = mail.name
+            else:
+                newname = mail.name + ':2,'
+            mail.rename(folder / 'cur' / newname)
+
+        # Index the stuff in cur.
         for mail in (folder / 'cur').iterdir():
             if str(mail) not in path_messageid:
                 with mail.open('rb') as fp:
