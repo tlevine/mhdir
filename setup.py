05cf9d12b78a6697074e0b09e8e2de3869259208
Thomas Levine <_@thomaslevine.com>
2015-12-19 16:30:15 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'mhdir',
+    version = '0.0.1',
+    description = 'Like MH but smaller and based on Maildir',
+    author = 'Thomas Levine',
+    author_email = '_@thomaslevine.com',
+    url = 'http://dada.pink/mhdir/',
+    entry_points = {'console_scripts': ['m = mhdir:cli']},
+    license = 'AGPL',
+    packages = ['mhdir'],
+    install_requires = [
+        'horetu',
+        'lxml',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+    ],
+)
